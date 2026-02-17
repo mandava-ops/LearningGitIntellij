653c17aae92fe059da4cbc13302a9536bf02b5e0
mandava-ops <mandavas2025@gmail.com>
2026-02-17 14:47:17 -0500

    Added - in place of +
    Added - in place of +

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass + Stream2Learn");
+        System.out.println("DevClass-Stream2Learn");
         //Checking from Stream2 Learn
     }
 }
