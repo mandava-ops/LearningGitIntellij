0ef031a3ef4cea69996a38182bb7fe5e9f3f0b90
mandava-ops <mandavas2025@gmail.com>
2026-02-17 14:34:46 -0500

    Update DevClass.java
    commit from account2, stream2learn branch

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,5 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass");
+        System.out.println("DevClass + Stream2Learn");
+        //Checking from Stream2 Learn
     }
 }
